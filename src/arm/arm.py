b2bca55cdeef77d15a8f44749841f2c4cea81833
Fabien DEPRAETRE <depraetref@gmail.com>
2014-01-17 14:43:19 +0100

    Commit for merge

--- src/arm/arm.py
+++ src/arm/arm.py
@@ -202,23 +202,27 @@ if __name__ == '__main__':
     motorControler.setAllSpeed(100)
     plotter = plot.Ploting()
     initTime = time.time()
-
+    i = 0
+  
     while True: 
-      key = raw_input("Do you want to move or plot?" + "\n\r" + "\t - (p) plot \r\n \t - (m) move \r\n")
-      if key == 'm':
+#      key = raw_input("Do you want to move or plot?" + "\n\r" + "\t - (p) plot \r\n \t - (m) move \r\n")
+#      if key == 'm':
+       if i < 10:
         # First tests
-        motorControler.setAllMotorsByName([["bottom",220],["middle",230],["head",250],["top",150],["bowl",200]])
+        motorControler.setAllMotorsByName([["bottom",140],["middle",230],["head",250],["top",150],["bowl",200]])
         out = motorControler.readAllMotors()
         plotter.addNewVal(out,time.time()-initTime)
         # Second tests
-        motorControler.setAllMotorsByName([["bottom",200],["middle",160],["head",150],["top",100],["bowl",300]])
+        motorControler.setAllMotorsByName([["bottom",160],["middle",160],["head",150],["top",100],["bowl",300]])
         out = motorControler.readAllMotors()
         plotter.addNewVal(out,time.time() - initTime)
         # Third tests
-        motorControler.setAllMotorsByName([["bottom",180],["middle",160],["head",150],["top",200],["bowl",300]])
+        motorControler.setAllMotorsByName([["bottom",140],["middle",160],["head",150],["top",200],["bowl",300]])
         out = motorControler.readAllMotors()
         plotter.addNewVal(out,time.time() - initTime)
-      elif key == 'p':
+        i += 1
+#      elif key == 'p':
+       else:
         plotter.plot()
         break
 
