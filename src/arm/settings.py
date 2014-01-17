09d8a698cde2dcaf927d1c48110b6e40084e41fb
Thibaud Lambert <thibaud.lambert@gmail.com>
2014-01-17 11:35:17 +0100

    Move Settings in another file

---
+++ src/arm/settings.py
@@ -0,0 +1,78 @@
+import yaml
+import os
+import subprocess
+
+def validateInput(userInput, rangeMin, rangeMax):
+    '''
+    Returns valid user input or None
+    '''
+    try:
+        inTest = int(userInput)
+        if inTest < rangeMin or inTest > rangeMax:
+            print "ERROR: Value out of range [" + str(rangeMin) + '-' + str(rangeMax) + "]"
+            return None
+    except ValueError:
+        print("ERROR: Please enter an integer")
+        return None
+    
+    return inTest
+
+
+class motorSettings:
+    def __init__(self, path = None):
+        self.settingPath = "settings.yaml"
+        if path != None :
+            self.settingPath = path
+        with open(self.settingPath, 'r') as fh:
+               self.settings = yaml.load(fh)
+
+    def create(self):
+        self.settings = {}
+        if os.name == "posix":
+            portPrompt = "Which port corresponds to your USB2Dynamixel? \n"
+            # Get a list of ports that mention USB
+            try:
+                possiblePorts = subprocess.check_output('ls /dev/ | grep -i usb',
+                                                        shell=True).split()
+                possiblePorts = ['/dev/' + port for port in possiblePorts]
+            except subprocess.CalledProcessError:
+                sys.exit("USB2Dynamixel not found. Please connect one.")
+                
+            counter = 1
+            portCount = len(possiblePorts)
+            for port in possiblePorts:
+                portPrompt += "\t" + str(counter) + " - " + port + "\n"
+                counter += 1
+            portPrompt += "Enter Choice: "
+            portChoice = None
+            while not portChoice:                
+                portTest = raw_input(portPrompt)
+                portTest = validateInput(portTest, 1, portCount)
+                if portTest:
+                    portChoice = possiblePorts[portTest - 1]
+
+        else:
+            portPrompt = "Please enter the port name to which the USB2Dynamixel is connected: "
+            portChoice = raw_input(portPrompt)
+    
+        self.settings['port'] = portChoice
+        
+        # Baud rate
+        baudRate = None
+        while not baudRate:
+            brTest = raw_input("Enter baud rate [Default: 1000000 bps]:")
+            if not brTest:
+                baudRate = 1000000
+            else:
+                baudRate = validateInput(brTest, 9600, 1000000)
+                    
+        self.settings['baudRate'] = baudRate
+        
+        # Save the output settings to a yaml file
+        with open(self.settingPath, 'w') as fh:
+            yaml.dump(self.settings, fh)
+        pass
+    
+    def get(self):
+        return self.settings
+ 
