128c5619ebcb84f84d01e4174e20f799e1985ac5
Fabien DEPRAETRE <depraetref@gmail.com>
2014-01-21 23:36:34 +0100

    Minor modification on trajectory

---
+++ src/test/testTraj.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(0,"../control/")
+import motorControl
+sys.path.insert(0, "../settings/")
+import settings
+sys.path.insert(0,"../traj/")
+import trajectoryController
+import time
+import cv2
+import select
+
+
+trajectoryController = trajectoryController.TrajectoryController(5,2)
+
+motorSettings = settings.motorSettings()
+motorControler = motorControl.MotorControl(motorSettings.get())
+motorControler.setAllSpeed(100)
+valMotor = motorControler.readMotorByName([["head"],["top"],["bowl"]])
+
+
+trajectoryController.set(valMotor,[40,150,20])
+while time.time()- trajectoryController.timeInit < trajectoryController.timef :
+  valMotor = motorControler.readMotorByName([["head"],["top"],["bowl"]])
+  motorControler.setMotorsByName([["head",trajectoryController.position[0]],["top",trajectoryController.position[1]],["bowl",trajectoryController.position[2]]])
+  trajectoryControler.update(trajectoryController.position)
+
