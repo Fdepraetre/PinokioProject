d29208cc41054885815db011f66e41dee772ff61
Fabien DEPRAETRE <depraetref@gmail.com>
2014-01-22 15:37:27 +0100

    Adding Led control with face detection

--- src/arduino/python2arduino.py
+++ src/arduino/python2arduino.py
@@ -10,4 +10,16 @@ class Arduino:
     blue = str(chr(b))
     green = str(chr(g))
   
-    self.ser.writelines(red+green+blue) # send color to the Arduino     
+    self.ser.writelines(red+green+blue) # send color to the Arduino
+  def greenLight(self):
+    self.lightLed(0,255,0)
+
+  def redLight(self):
+    self.lightLed(255,0,0)
+
+  def blueLight(self):
+    self.lightLed(0,0,255)
+
+
+
+
