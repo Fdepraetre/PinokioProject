2e1e0f57785ecb9b3fd56bf80ece2bb62b1cfbb0
Fabien DEPRAETRE <depraetref@gmail.com>
2014-01-23 18:06:35 +0100

    Adding commentary

--- src/prog/behaviorClient.py
+++ src/prog/behaviorClient.py
@@ -14,29 +14,44 @@ import python2arduino
 import cv2
 
 
+# Initialisation of motor with detection 
 motorSettings = settings.MotorSettings()
 motorControler = motorControl.MotorControl(motorSettings.get())
 faceStream = FaceDetection.FaceStream(1)
 
+# Init Face detection 
 faceSearchBehavior = searchFaces.FaceSearch(motorControler,faceStream,60)
+# Start Face Search 
 faceSearchBehavior.start()
 
 arduino = python2arduino.Arduino('/dev/ttyACM0')
 
+#Camera 
 precision = 0.1
 res = [360,240]
 apertureAngle = [50.,30.]
+
+# Time out for loosing 
+thresholdFace = 2
+
+# Initialisation for the camera control
 camera = cameraControl.CameraControl(motorControler, faceStream, precision, res, apertureAngle)
 
 exit = False
-thresholdFace = 2
+
+
 state = "faceSearch"
+
+# Set the light to red value on led circle
 arduino.redLight()
+
 while not exit :
   if state == "faceFollow" : 
+    # A face is detected
     motorControler.setAllSpeed(100)
     lastTime = time.time()
     timeOut = 0
+    # Time out to search a face
     while timeOut < thresholdFace :
       frame =	faceStream.nextFrame()
       if faceStream.isFaceDetected():
@@ -48,10 +63,12 @@ while not exit :
         timeOut += currentTime - lastTime
         lastTime = currentTime
         arduino.redLight()
+      # Display the frame
       faceStream.display()
       key = cv2.waitKey(10)
       camera.updateControl()
     state = "faceSearch"     
   elif state == "faceSearch" :
+    # Start searching faces
     faceSearchBehavior.start()
     state = "faceFollow"     
